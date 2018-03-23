d66d57df8f7c9e717ac4a46a42f75d7ed093fa33
Faizan Ubuntu <mirzafaizanejaz@gmail.com>
2018-03-24 01:49:06 +0500

    Completed update user by ID

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -71,7 +71,40 @@ exports.findOne = function(req , res){
 
 //update a user with userID
 exports.update = function(req , res){
+    User.findById(req.params.userId,function(err,user){
+        if(err){
+            console.log(err);
+            if(err.kind === 'ObjectId'){
+                return res.status(404).send({
+                    message: "User not found with Id "+req.params.userId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retriving user with id "+req.params.userId
+            });
+        }
+        if(!user){
+            return res.status(404).send({
+                message: "User not found with Id "+req.params.userId
+            });
+        }
 
+        //updating the searched obj
+        user.fname = req.body.fname;
+        user.lname = req.body.lname;
+        user.cnic= req.body.cnic;
+        user.email= req.body.email;
+
+        user.save(function(err,data){
+            if(err){
+                res.status(500).send({
+                    message: "Could not update user with ID "+req.params.userId 
+                });
+            }else{
+                res.send(data);
+            }
+        });
+    });
 };
 
 //delete a user with userID
